1590a296f5194991b94434d1f2c76d52009d36a1
PeteGrapentien <PeteGrapentien@gmail.com>
2018-01-22 07:38:04 -0500

    Added a TODO for next update

--- app/src/main/java/com/retrofitpracticejava/pete/retrofitpracticejava/GithubServiceInterface.java
+++ app/src/main/java/com/retrofitpracticejava/pete/retrofitpracticejava/GithubServiceInterface.java
@@ -3,7 +3,9 @@ package com.retrofitpracticejava.pete.retrofitpracticejava;
 import java.util.List;
 
 import retrofit2.Call;
+import retrofit2.http.Body;
 import retrofit2.http.GET;
+import retrofit2.http.POST;
 import retrofit2.http.Path;
 
 /**
@@ -13,4 +15,8 @@ import retrofit2.http.Path;
 public interface GithubServiceInterface {
     @GET("users/{user}/repos")
     public Call<List<Repo>> listRepos(@Path("user") String user);
+
+    //TODO: Write a create account method (or something similar)
+//    @POST
+//    public Call<User> createAccount(@Body User user);
 }
